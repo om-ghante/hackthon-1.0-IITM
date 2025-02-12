0b6633b2b5a680b5cfef4d7b21b86fe4d256354f
om-ghante <omghante.official@gmail.com>
2025-02-12 20:23:23 +0530

    Another Commit

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react"
-import LogoImage from "../assets/logo.png"
+import KanhaHLogoImage from "../assets/kanhaH.png"
+import SundarbanHLogoImage from "../assets/sundarbanH.png"
 
 const Home = () => {
   return (
@@ -8,8 +9,8 @@ const Home = () => {
         <h1 className="text-6xl font-bold mb-4">HACKATHON</h1>
         <h2 className="text-3xl mb-8">IITM (Kanha + Sudarban House)</h2>
         <div className="flex justify-center space-x-4 mb-8">
-          <img src={LogoImage} alt="Image 1" className="w-24 h-24 rounded-full" />
-          <img src={LogoImage} alt="Image 2" className="w-24 h-24 rounded-full" />
+          <img src={KanhaHLogoImage} alt="Image 1" className="w-24 h-24 rounded-full" />
+          <img src={SundarbanHLogoImage} alt="Image 2" className="w-24 h-24 rounded-full" />
         </div>
         <button className="bg-sky-500 hover:bg-sky-600 text-white font-bold py-2 px-4 rounded">Register Now</button>
       </div>
