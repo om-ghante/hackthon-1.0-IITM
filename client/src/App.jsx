8c7a421c7c5491d04467d10fd92ab342d1c6be97
om-ghante <omghante.official@gmail.com>
2025-02-24 20:01:49 +0530

    Another Commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,11 +6,13 @@ import Contact from "./components/ContactUs"
 import Sponsors from "./components/Sponsors"
 import FAQ from "./components/FAQ"
 import Footer from "./components/Footer"
+import Navbar from "./components/NavBar"
 
 const App = () => {
   return (
     <div className="min-h-screen bg-gradient-to-br from-purple-900 via-black to-pink-900 text-white">
       <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }}>
+        <Navbar />
         <Hero />
         <About />
         <Sponsors />
